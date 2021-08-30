d6c20e58247cb99c1b85f905bcb2c3c3f239cf6b
dongseoki <43495319+dongseoki@users.noreply.github.com>
2021-08-30 21:17:04 +0900

    react 연동해보았습니다.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,54 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+const [message, setMessage] = useState("");
+useEffect(() => {
+fetch('/api/hello')
+.then(response => response.text())
+.then(message => {
+setMessage(message);
+});
+},[])
+return (
+<div className="App">
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo"/>
+<h1 className="App-title">{message}</h1>
+</header>
+<p className="App-intro">
+testTo get started, edit <code>src/App.js</code> and save to reload.
+</p>
+</div>
+)
 }
-
 export default App;
+
+// // 출처: https://sundries-in-myidea.tistory.com/71 [얇고 넓은 개발 블로그]
--- src/main/java/com/lds/trackdayb/controller/HomeController.java
+++ src/main/java/com/lds/trackdayb/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 
-    @GetMapping("/")
-    public String home(){
-        return "redirect:/main/main";
-    }
+    // @GetMapping("/")
+    // public String home(){
+    //     return "redirect:/main/main";
+    // }
 }
