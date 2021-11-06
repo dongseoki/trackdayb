620f98ca0822b4b5d0b22f04a930bf8f2f232384
dongseoki <43495319+dongseoki@users.noreply.github.com>
2021-11-06 17:31:00 +0900

    Release 0.3.6   - 목표 수정 API : 색이 인자로 없을 경우, 수정하지 않도록 수정. 반환값에 goalTitleInfo 포함하도록 조치.

--- src/main/java/com/lds/trackdayb/mvo/ActivityMVO.java
+++ src/main/java/com/lds/trackdayb/mvo/ActivityMVO.java
@@ -3,6 +3,8 @@ package com.lds.trackdayb.mvo;
 import com.lds.trackdayb.dto.ActivityDTO;
 import com.lds.trackdayb.dto.GoalDTO;
 
+import lombok.Data;
+@Data
 public class ActivityMVO extends ActivityDTO {
     GoalDTO goalTitleInfo = new GoalDTO();
 }
