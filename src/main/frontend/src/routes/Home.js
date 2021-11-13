12804e6ec9d3de404813fef0ed0a26fb770661ce
eugenekk <08youjin12@gmail.com>
2021-11-13 15:50:25 +0900

    main-page 생성

--- src/main/frontend/src/routes/Home.js
+++ src/main/frontend/src/routes/Home.js
@@ -1,21 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import "./Home.css"
 
 function Home () {
-    const [message, setMessage] = useState("");
-    useEffect(() => {
-        fetch('/api/hello')
-        .then(response => response.text())
-        .then(message => {
-            setMessage(message);
-        });
-    },[])
     return (
         <div className="home">
-            <h1 className="App-title">{message}</h1>
-            <p>Track</p>
-            <p>Your</p>
-            <p>Day</p>
+            <div className="main-slogan">
+                <p>Track</p>
+                <p>Your</p>
+                <p>Day</p>
+            </div>
+            <div className="main-illustrations">
+                <img className="checking-box-svg" src="img/undraw_checking_boxes_re_9h8m.svg" alt="checking box"></img>
+                <img className="dividing-time-svg" src="img/undraw_personal_goals_re_iow7.svg" alt="Time management"></img>
+                <img className="progress-bar-svg" src="img/undraw_progress_overview_re_tvcl.svg" alt="Progress bar"></img>
+                <img className="bulletin-board-svg" src="img/undraw_timeline_re_aw6g.svg" alt="Bulletin board"></img>
+            </div>
         </div>
     )
 }
