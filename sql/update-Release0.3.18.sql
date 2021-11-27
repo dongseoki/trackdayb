f0c6f4cc483278001ca1dc2af218ad7962b251ba
dongseoki <43495319+dongseoki@users.noreply.github.com>
2021-11-27 11:42:53 +0900

    Release 0.3.18   - 부모 목표를 삭제하여 자식이 1차 부모가 되는 경우 → 컬러를 임의로 넣도록 수정함.   - 목표 수정 - 주기성정보의 카운트 값이 ""(빈문자열)일 경우 수정 안되던 문제 해결.   - 목표 수정 이나 추가 할때 시분초 값을 무시하고 수정 및 저장 하는 식으로 변경.

---
+++ sql/update-Release0.3.18.sql
@@ -0,0 +1,9 @@
+UPDATE goal as g1
+INNER JOIN goal as g2
+on g1.goal_id =g2.goal_id 
+SET g1.start_datetime = DATE_FORMAT(g2.start_datetime, '%Y-%m-%d 00:00:00')
+
+UPDATE goal as g1
+INNER JOIN goal as g2
+on g1.goal_id =g2.goal_id 
+SET g1.end_datetime = DATE_FORMAT(g2.end_datetime , '%Y-%m-%d 23:59:59')
