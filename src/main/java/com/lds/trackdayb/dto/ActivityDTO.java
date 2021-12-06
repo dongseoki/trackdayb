4007aa777c59644d73330201fdfc05b1314af2ee
dongseoki <43495319+dongseoki@users.noreply.github.com>
2021-12-06 20:35:38 +0900

    Release 0.3.20   -  mysql 쿼리 update문을 각각 따로 실행해야 처리되야하는 문제 해결.   -  목표 관리 - 정렬 조건(목표 모아보기, 및 우측4가지 선택)을 중첩하여 적용 (goalYn orderColumn명시?  -> 정렬 방식 Y Y --> 최상위 목표 asc / 컬럼명 정렬 Y N --> 최상위 목표 asc / 계층형 정렬 N Y --> 컬럼명 정렬 N N --> 수정일시 desc 정렬)   -  목표 카드 색깔 표시를 위한 새로운 컬럼 제공   -  update 컬럼 설정 확인 및 수정.

--- src/main/java/com/lds/trackdayb/dto/ActivityDTO.java
+++ src/main/java/com/lds/trackdayb/dto/ActivityDTO.java
@@ -7,7 +7,7 @@ public class ActivityDTO {
     private String activityId = "";  // INT NOT NULL AUTO_INCREMENT COMMENT '활동 기록 ID'
     private String memberSerialNumber = "";  // INT NOT NULL  COMMENT '멤버테이블 일련번호(AUTOINCREAMENT)'
     private String goalId = "";  // INT   COMMENT ''
-    private String title = "";  // VARCHAR(50) NOT NULL  COMMENT '제목'
+    private String title;  // VARCHAR(50) NOT NULL  COMMENT '제목'
     private String startDatetime = "";  // DATETIME NOT NULL  COMMENT '시작 시간값.(시분)'
     private String endDatetime = "";  // DATETIME NOT NULL  COMMENT '종료 시간.(시분) 비고 선택 화면에 다음날짜의 시간까지 선택 가능'
     private String content = "";  // TEXT   COMMENT '활동 내용'
