78a6551c0359b803c8e3ad34d2e748b8f8fcf8fe
eugenekk <08youjin12@gmail.com>
2021-12-13 23:43:14 +0900

    가이드 사진 업로드, 폼 라벨 수정

--- src/main/frontend/src/context/GoalSearchTitleListContext.js
+++ src/main/frontend/src/context/GoalSearchTitleListContext.js
@@ -18,6 +18,7 @@ export const GoalSearchTitleListProvider = (props) =>{
                 }
               });
             setGoalSearchTitleList(result.data.goalTitleList);
+            console.log('result_test', result.data.goalTitleList)
           } catch(err) {
             console.error(err);
           }
