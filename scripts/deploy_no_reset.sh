dfe9293958280018806c59fc69ce9e484d01930e
dongseoki <start0is5half@gmail.com>
2021-12-25 20:12:28 +0900

    deploy scripts push

---
+++ scripts/deploy_no_reset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/apps
+PROJECT_NAME=trackdayb
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+#echo "> Git reset --hard"
+#git reset --hard
+
+echo "> Git Pull"
+
+git pull
+
+echo "> 프로젝트 Build 시작"
+chmod 777 gradlew
+./gradlew build
+
+echo "> Build 파일 복사"
+
+cp ./build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -2 $CURRENT_PID"
+    kill -9 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+# JAR_NAME=$(ls $REPOSITORY/ |grep jar | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/ |grep ${PROJECT_NAME}.*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME &
