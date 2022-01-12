541fd4c4c713a505bd1f556972b772641bea7e84
eugenekk <08youjin12@gmail.com>
2022-01-12 13:53:19 +0900

    jsEncrypt 설치, RSA 암호/복호 테스트

--- src/main/frontend/src/routes/Login.js
+++ src/main/frontend/src/routes/Login.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "./Login.css";
 import TextField from '@material-ui/core/TextField';
 import axios from "axios";
@@ -6,9 +6,41 @@ import { AuthContext } from "../context/AuthContext";
 import {useHistory} from "react-router-dom";
 import { toast } from 'react-toastify';
 import useTitle from '../hooks/useTitle';
+import JSEncrypt from 'jsencrypt';
 
 
 function Login() {
+  const publicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN
+  FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76
+  xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4
+  gwQco1KRMDSmXSMkDwIDAQAB`
+
+  const privateKey = `MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ
+  WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR
+  aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB
+  AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv
+  xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH
+  m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd
+  8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF
+  z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5
+  rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM
+  V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe
+  aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil
+  psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz
+  uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876`
+
+  useEffect(()=> {
+    let rsaEncrypt = new JSEncrypt();
+    rsaEncrypt.setPublicKey(publicKey);
+    let rsaEncryptedData = rsaEncrypt.encrypt("hello");
+    console.log("rsaEncryptedData", rsaEncryptedData);
+
+    let rsaDecrypt = new JSEncrypt();
+    rsaDecrypt.setPrivateKey(privateKey);
+    let rsaDecryptedData = rsaDecrypt.decrypt(rsaEncryptedData);
+    console.log("rsaDecryptedData", rsaDecryptedData)
+  },[])
+
   const titleUpdater = useTitle("trackDay");
   setTimeout(()=>titleUpdater("로그인"), 100);
 
