cb375eb9583e7a2be299b477cb815bc3ed664722
eugenekk <08youjin12@gmail.com>
2022-01-16 20:51:19 +0900

    rsa 암호화 적용

--- src/main/frontend/src/routes/Login.js
+++ src/main/frontend/src/routes/Login.js
@@ -10,61 +10,50 @@ import JSEncrypt from 'jsencrypt';
 
 
 function Login() {
-  const publicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN
-  FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76
-  xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4
-  gwQco1KRMDSmXSMkDwIDAQAB`
-
-  const privateKey = `MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ
-  WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR
-  aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB
-  AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv
-  xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH
-  m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd
-  8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF
-  z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5
-  rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM
-  V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe
-  aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil
-  psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz
-  uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876`
-
-  useEffect(()=> {
-    let rsaEncrypt = new JSEncrypt();
-    rsaEncrypt.setPublicKey(publicKey);
-    let rsaEncryptedData = rsaEncrypt.encrypt("hello");
-    console.log("rsaEncryptedData", rsaEncryptedData);
-
-    let rsaDecrypt = new JSEncrypt();
-    rsaDecrypt.setPrivateKey(privateKey);
-    let rsaDecryptedData = rsaDecrypt.decrypt(rsaEncryptedData);
-    console.log("rsaDecryptedData", rsaDecryptedData)
-  },[])
-
   const titleUpdater = useTitle("trackDay");
   setTimeout(()=>titleUpdater("로그인"), 100);
+
+  const [ publicKey, setPublicKey ] = useState('')
+
+  // 공개키 요청함수
+  const fetchPublickKey = async () => {
+    try {
+      const result = await axios.get('/member/requestpublickey')
+      setPublicKey(result.data.publicKeyInfo.publicKeyStr)
+    }catch(err) {
+      console.error(err)
+    }
+  }
+  // 페이지로드시 함수 호출
+  useEffect(()=> {
+    fetchPublickKey();
+  },[])
 
   const [memberId, setMemberId] = useState("");
   const [password, setPassword] = useState("");
   const [ , setCurUser ] = useContext(AuthContext);
   const history = useHistory();
 
-  
   const validateForm = ()=>{
     return memberId.length > 0 && password.length > 0;
   }
 
+  // 로그인 제출
   const handleSubmit = async (evt) =>{
     evt.preventDefault();
     if(!validateForm()){
       toast.error("올바른 정보를 입력하세요")
     }
+    // 데이터 암호화
+    let rsaEncrypt = new JSEncrypt();
+    rsaEncrypt.setPublicKey(publicKey);
+    let rsaEncryptedMemberId = rsaEncrypt.encrypt(memberId);
+    let rsaEncryptedPassword = rsaEncrypt.encrypt(password);
 
     const formData = {
-      memberId,
-      password,
+      memberId : rsaEncryptedMemberId,
+      password : rsaEncryptedPassword,
     };
-
     try{
       const result = await axios.post("/member/login", formData);
       //현재 유저 설정
@@ -79,6 +68,7 @@ function Login() {
       
     }catch(err){
       toast.error(`올바른 정보를 입력하세요 (${err.response.statusText})`)
+      fetchPublickKey(); // 실패시 새로운 키 요청
     }
   }
 
